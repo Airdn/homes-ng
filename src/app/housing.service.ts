adaefaa4d897d1af382c303b1a0e15a3cd331cff
Denis <denisdebron@gmail.com>
2025-04-29 00:13:28 +0300

    Add files via upload

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,29 +5,29 @@ import { Housinglocation } from "./housinglocation";
   providedIn: 'root'
 })
 export class HousingService {
-  // url = 'https://raw.githubusercontent.com/Airdn/homes-ng/refs/heads/main/db.json';
-  url = 'http://localhost:3000/location';
+  url = 'https://raw.githubusercontent.com/Airdn/homes-ng/refs/heads/main/db.json';
+  // url = 'http://localhost:3000/location';
 
   constructor() { }
 
   async getAllHousingLocations() : Promise<Housinglocation[]> {
-    const data = await fetch(this.url);
-    return await data.json() ?? [];
     // const data = await fetch(this.url);
-    // const jsonResponse = await data.json();
-    // return jsonResponse.location ?? [];
+    // return await data.json() ?? [];
+    const data = await fetch(this.url);
+    const jsonResponse = await data.json();
+    return jsonResponse.location ?? [];
   }
 
   async getHousingLocationById(id: Number): Promise<Housinglocation | undefined> {
-    const data = await fetch(`${this.url}/${id}`);
-    return await data.json() ?? {};
-    // const data = await fetch(this.url);
-    // const jsonResponse = await data.json();
+    // const data = await fetch(`${this.url}/${id}`);
+    // return await data.json() ?? {};
+    const data = await fetch(this.url);
+    const jsonResponse = await data.json();
 
     // Находим нужную карточку по id из массива location
-    // const location = jsonResponse.location.find((item: Housinglocation) => item.id === id);
-    // // Возвращаем найденную карточку или undefined, если не найдено
-    // return location ?? undefined;
+    const location = jsonResponse.location.find((item: Housinglocation) => item.id === id);
+    // Возвращаем найденную карточку или undefined, если не найдено
+    return location ?? undefined;
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {
